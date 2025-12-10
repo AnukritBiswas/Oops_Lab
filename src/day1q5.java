d27c43fd9952587fbd3625fc046a79364d3bd534
AnukritBiswas <24051468@kiit.ac.in>
2025-12-10 13:33:07 +0530

    new

---
+++ src/day1q5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class NameFormat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        String firstName = sc.nextLine();
+
+        System.out.print("Enter Second Name: ");
+        String lastName = sc.nextLine();
+
+        System.out.println("Output: " + lastName + " " + firstName);
+    }
+}
